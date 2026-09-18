611886ea2563b31b5f9a1d8f5779a70b203aea0e
asivitskis <sivitskis.alex@gmail.com>
2026-09-17 17:26:18 -0700

    update basemap

--- map.js
+++ map.js
@@ -1,7 +1,6 @@
 // Build your map here!
 let map = L.map("map", {center: [32.77, -108.278], zoom: 15})
 
-// 2. Add a basemap tile layer
 
 // Add additional basemap
 let alt_base = L.tileLayer.provider("OpenTopoMap").addTo(map);
